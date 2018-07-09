d9c2c34e9409d2e0d3d1f6da945e6d8effac6265
srikanth <srikanthsree50@gmail.com>
2018-07-09 21:56:43 +0530

    commited

--- readme.js
+++ readme.js
@@ -1,7 +1,9 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="style.css" rel="stylesheet">
+	
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+<link href="style.css" rel="stylesheet"/>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -37,8 +39,8 @@ class="glyphicon glyphicon-list"> </span>
 	</div>
 	<div class="panel-body">
 		content.. here..		
-    <div>
-<div>
+    </div>
+</div>
 ------------------
 <table class="table table-striped">
 	<thead>
