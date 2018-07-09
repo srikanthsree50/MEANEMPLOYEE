121abc6730f095401e76b73359825b8c3036e58d
srikanth <srikanthsree50@gmail.com>
2018-07-09 22:15:27 +0530

    commit

--- readme.js
+++ readme.js
@@ -1,6 +1,6 @@
 
 	
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
 <link href="style.css" rel="stylesheet"/>
